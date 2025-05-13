adf1df354df0975327138a38c4f20a2823d4d253
invictus1032 <invictus1032@gmail.com>
2025-05-13 15:35:49 +0700

    build: update sourceDir to point to './src' and remove moved files from ignoredFiles

--- web-ext-config.mjs
+++ web-ext-config.mjs
@@ -1,13 +1,10 @@
 export default {
   verbose: true,
-  sourceDir: './', // Where your manifest.json and sources are
+  sourceDir: './src', // Where your manifest.json and sources are
   artifactsDir: './dist', // Where to save the zip file
   ignoreFiles: [
     '.git',
     'node_modules',
-    'README.md',
-    'LICENSE',
-    'web-ext-config.mjs',
   ],
   build: {
     overwriteDest: true,
